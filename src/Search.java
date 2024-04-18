7eebb2e8af7054ce9338b53daaef5dc1da7b6f52
montif16 <Lukas1532@hotmail.com>
2024-04-18 10:50:22 +0200

    Populated search with functions

--- src/Search.java
+++ src/Search.java
@@ -1,2 +1,18 @@
+import java.util.ArrayList;
+
 public class Search {
+    private ArrayList<Video> searchResults;
+   public void searchGenre(){
+
+   }
+   public void searchName(){
+
+   }
+   public void searchRating(){
+
+   }
+   public void searchReleaseDate(){
+
+   }
+
 }
