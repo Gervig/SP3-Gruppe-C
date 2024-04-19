8350a69816b3248f9967fe4febe23e85c06bab48
montif16 <Lukas1532@hotmail.com>
2024-04-19 10:57:55 +0200

    First iteration of search (It sucks)

--- src/Search.java
+++ src/Search.java
@@ -3,11 +3,31 @@ import java.util.Scanner;
 
 
 public class Search {
+   private String title;
+   private String releaseDate;
+   private String genre;
+   private String rating;
     private ArrayList<Video> csr;
+    FileIO reader = new FileIO();
     Scanner scan = new Scanner(System.in);
+    private void seperator(){
+       for (String line : reader.readFilmData("data/film.txt",100)){
+          String[] parts = line.split(";");
+           title = parts[0];
+           releaseDate = parts[1];
+           genre = parts[2];
+           rating = parts[3];
+       }
+    }
    public void searchGenre(){
       TextUI.displayMsg("Please enter the genre you want to search");
       String choice = scan.nextLine();
+      seperator();
+      if(genre.contains(choice)){
+         TextUI.displayMsg("Now playing: "+choice);
+      } else {
+         TextUI.displayMsg("No film or series was found within those search parameters, try another search");
+      }
 
       //Use scanner to gain user input which will be the search
       //Use string given by to search a file
