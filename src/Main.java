98aa821e8d5776a576ca8df4f6da53987f251dd1
montif16 <Lukas1532@hotmail.com>
2024-04-19 11:53:53 +0200

    Changed how to access Search, can now be used with any CSV file

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 public class Main {
     public Stream stream = new Stream();
+    static FileIO reader = new FileIO();
 
     public static void main(String[] args) {
         Search sear = new Search();
-        sear.searchGenre();
+        sear.searchGenre(reader.readFilmData("data/film.txt",100));
 
     }
 }
