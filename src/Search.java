3e4e4efc776b01604117284ebdaafda78b03dd62
Crispy212 <Christofferleisted@gmail.com>
2024-04-19 12:46:59 +0200

    Updated Stream v0.3

--- src/Search.java
+++ src/Search.java
@@ -1,35 +1,7 @@
-import java.util.ArrayList;
-import java.util.Scanner;
-
-
 public class Search {
-    private ArrayList<Video> csr;
-    Scanner scan = new Scanner(System.in);
-   public void searchGenre(){
-      TextUI.displayMsg("Please enter the genre you want to search");
-      String choice = scan.nextLine();
-
-      //Use scanner to gain user input which will be the search
-      //Use string given by to search a file
-      //Pull data from fileIO, data comes as CSV
-      //Separate the data from CSV to only gain data about genre
-      //Return full list of movies that correlates with the searched genre
 
 
-   }
-   public void searchName(){
-      //Repeat searchGenre() but for movie names
+    public Search(){
 
-   }
-   public void searchRating(){
-      //Repeat searchGenre() but for rating (Potentially a range)
-
-
-   }
-   public void searchReleaseDate(){
-      //Repeat searchGenre() but for release date (Potentially a range)
-
-
-   }
-
+    }
 }
--- src/Stream.java
+++ src/Stream.java
@@ -9,7 +9,11 @@ public class Stream {
     String seriesDataPath = "data\\series.txt";
     String filmDataPath = "data\\film.txt";
 
-    public void Stream(String name) {
+    protected Film film;
+
+    protected Series series;
+
+    public Stream(String name) {
         this.name = name;
         this.ui = new TextUI();
         this.io = new FileIO();
@@ -22,28 +26,46 @@ public class Stream {
     }
 
     private void setup() {
-        //readFilmData(filmDataPath);
-        //readSeriesData(seriesDataPath);
+        ArrayList<String> filmData = new ArrayList<>(io.readFilmData(filmDataPath, 100));
+        for (String s : filmData) {
+            String[] values = s.split(";");
+            String name = values[0];
+            String year = values[1];
+            String genre = values[2];
+            float rating = Float.parseFloat(values[3].trim());
+            Film film = new Film(name, year, genre, rating);
+        }
 
+        ArrayList<String> seriesData = new ArrayList<>(io.readSeriesData(seriesDataPath, 100));
+        for (String s : seriesData) {
+            String[] values = s.split(";");
+            String name = values[0];
+            String year = values[1];
+            String genre = values[2];
+            float rating = Float.parseFloat(values[3].trim());
+            String episodes = values[4];
+            Series series = new Series(name, year, genre, rating, episodes);
+
+        }
     }
 
-    public void runStartMenu() {
+    public void runDialog() {
         ui.displayMsg("Welcome to " + this.name);
         int action = 0;
         while (action != listOfActions.size()) {// the quit action is the last action
-           action = ui.promptChoice(listOfActions, "Choose action:");
+            action = ui.promptChoice(listOfActions, "Choose action:");
 
             switch (action) {
                 case 1:
                     //Login to existing user
                     this.loginUser();
-                    this.runDialog(); //Skal kalde på UserMenu (findes i domænemodellen, findes IKKE i classeDiagrammet)
+                    this.runStartMenu();
 
                     break;
                 case 2:
                     //Sign up new user
                     this.createUser();
-                    this.runDialog();
+                    this.runStartMenu();
                     break;
                 case 3:
                     //quit program
@@ -51,52 +73,80 @@ public class Stream {
 
                     break;
             }
+        }
+    }
 
-            public void createUser(){
-                String newUsername = promptText("Choose a username:");
-                if(!users.getUsername().equals(newUsername)){
-                    String newPassword = promptText("Choose a password:");
-                    User newUser = new User(newUsername, newPassword);
-                    users.add(newUser);
-            }else{
-                System.out.println("Username already in use, please choose a different username:");
-            }
+    public User createUser() {
+        String newUsername = promptText("Choose a username:");
+        if (!users.getUsername().equals(newUsername)) {
+            String newPassword = promptText("Choose a password:");
+            User newUser = new User(newUsername, newPassword);
+            users.add(newUser);
+            return newUser;
+        } else {
+            System.out.println("Username already in use, please choose a different username:");
+            return null;
+        }
 
-            }
+    }
 
-            public void loginUser() {
+    public User loginUser() {
 
-                System.out.println("write username and password");
+        boolean isloggedIn = false;
+        String username = promptText("Please write username:");
+        // String password too
+        while (!isloggedIn) {
+            for (User u : users) {
+                if (u.getusername.equals(username)) {
+                    User user = u;
 
-                boolean isloggedIn = false;
-                String username = promptText("Please type your username:");
-                // String password too
-                while (!isloggedIn) {
-                    for (User u : users) {
-                        if (u.getusername.equals(username)) {
-                            User user = u;
-
-                            if (user.getPassword.equals(passwordPromptFromUserInput)) {
-                                System.out.println("logged in xd");
-                                isloggedIn = true;
-                                return;
-                            } else {
-                                sout
-                            }
-
-                        }
+                    if (user.getPassword.equals(promptText("Please write password:"))) {
+                        System.out.println("logged in xd");
+                        isloggedIn = true;
+                        return user;
+                    } else {
+                        System.out.println("Wrong password please try agian:");
+                        //Lav evt en back option
                     }
 
                 }
             }
 
-            public void runDialog () {
-
-            }
-
-            private void quitProgram () {
-
-            }
         }
+    }
+
+    public void runStartMenu() {
+        int choice = 0;
+        switch (choice) {
+            case 1:
+                //Search for movie / Series
+                Search search = new Search();
+
+                break;
+            case 2:
+                //View saved movies / series
+                currentUser.viewSaved
+
+                break;
+            case 3:
+                //View watch historie
+                currentUser.viewSaved
+
+                break;
+            case 4:
+                //logout
+                currentUser = null;
+                this.runDialog();
+                break;
+            case 5:
+                //quit
+                this.quitProgram();
+                break;
+        }
+
+    }
+
+    private void quitProgram() {
+        io.saveData(this.currentUser, );
     }
 }
