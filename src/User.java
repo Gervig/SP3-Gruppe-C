a10b74e31647f22efbefb860f5c2443cea5e7ed2
montif16 <Lukas1532@hotmail.com>
2024-04-23 12:17:34 +0200

    Changes by rfwma

--- src/User.java
+++ src/User.java
@@ -8,6 +8,7 @@ public class User {
     private ArrayList<Video> savedVideo;
     private String newPassword;
 
+
     public User(String name, String newPassword) {
         this.name = name;
         this.savedVideo = new ArrayList<>();
@@ -17,7 +18,7 @@ public class User {
     }
 
 //methods
-
+//vi skal have det over til objekter i stedet for strenge og så få det enkelte objekt derhen
     //This method is used to save a list of videos that the user has seen.
         public void watchedFilm(Film film){    //needs stream parameter for stream object to be visible for the method
         seenFilm.add(film);
@@ -51,5 +52,4 @@ public class User {
     public ArrayList<Video> getSavedVideo() {
         return savedVideo;
     }
-
     }
